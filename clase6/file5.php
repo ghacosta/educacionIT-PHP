a21e845bd21b69415694752a62d3c2fc4be9ba77
Guillermo Acosta <guillermo.acosta@tarmac.io>
2018-07-03 22:05:46 -0300

    trim word

--- clase6/file5.php
+++ clase6/file5.php
@@ -1,10 +1,29 @@
 <?php
-$pizza  = "porción1 porción2 porción3 porción4 porción5 porción6";
-$porciones = explode(" ", $pizza);
+// $pizza  = "porción1 porción2 porción3 porción4 porción5 porción6";
+// $porciones = explode(" ", $pizza);
 
-print_r($porciones);
+// print_r($porciones);
 
 // a partir de un archivo (de 4 lineas y 5 palabras por linea) 
 // leer cada linea del archivo y mostrar por pantalla
 // aquellas palabras que tengan mas de 6 caracteres 
 // a esas palabras mostrarlas en mayusculas.
+
+//fopen(, 'r'); +
+//fgets
+//explode
+//echo 
+//strtoupper
+
+$nombre = 'PHP.txt';
+$archivo = fopen($nombre, 'r');
+
+while (!feof($archivo)){
+  $linea = fgets($archivo);
+  $palabras = explode(' ', $linea);
+  foreach ($palabras as $palabra) {
+    if(strlen(trim($palabra)) > 6) {
+      echo strtoupper($palabra);
+    }
+  }
+}
