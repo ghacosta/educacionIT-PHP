f61667eb8156cbce35c7dbff3b82f4d897eabd5d
Guillermo Acosta <guillermo.acosta@tarmac.io>
2018-07-10 18:19:53 -0300

    class 7th and rename folders

---
+++ clase07/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname = 'localhost';
+$dbname   = 'comercioit';
+$username = 'root';
+$password = '';
+try {
+	$conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+	$sql = 'SELECT Nombre, Precio, Stock FROM Productos';
+
+	$conn->prepare($sql);
+
+	foreach ($conn->query($sql) as $valor) {
+		echo $valor['Nombre'];
+	}
+
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
